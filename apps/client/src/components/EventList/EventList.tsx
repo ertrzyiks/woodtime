7a3670875c6a9c9151cd9d18bcdc9ca9b6daae92
Joanna Derks <joanna.derks@schibsted.pl>
2021-06-28 22:01:54 +0200

    allow deleting event

--- apps/client/src/components/EventList/EventList.tsx
+++ apps/client/src/components/EventList/EventList.tsx
@@ -1,8 +1,18 @@
 import React, { useContext } from 'react';
-import { Fab, List, ListItem, ListItemText } from '@material-ui/core';
+import {
+  createStyles,
+  Fab,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import ClearIcon from '@material-ui/icons/Clear';
 import { Link } from 'react-router-dom';
-import { StorageContext } from '../Storage/Storage';
+import { ActionsContext, StorageContext } from '../Storage/Storage';
 import format from 'date-fns/format';
 
 const Event = ({
@@ -19,7 +29,29 @@ const Event = ({
   </ListItem>
 );
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    wrapper: {
+      display: 'flex',
+      width: '100%',
+    },
+    deleteIcon: {
+      // position: 'absolute',
+      // top: -3,
+      // right: -5,
+    },
+  })
+);
+
 const EventList = () => {
+  const actions = useContext(ActionsContext);
+
+  const handleDeleteClick = (eventId: number) => {
+    actions?.deleteEvent(eventId);
+  };
+
+  const classes = useStyles();
+
   const { events } = useContext(StorageContext);
   return (
     <div>
@@ -27,12 +59,22 @@ const EventList = () => {
         {events
           .sort((a: { id: number }, b: { id: number }) => b.id - a.id)
           .map(({ id, name, createdAt }) => (
-            <Event
-              key={id}
-              id={id}
-              name={name}
-              createdAt={format(new Date(createdAt), 'dd/MM/yyyy')}
-            />
+            <div className={classes.wrapper}>
+              <Event
+                key={id}
+                id={id}
+                name={name}
+                createdAt={format(new Date(createdAt), 'dd/MM/yyyy')}
+              />
+              <div className={classes.deleteIcon}>
+                <IconButton
+                  aria-label="delete"
+                  onClick={() => handleDeleteClick(id)}
+                >
+                  <ClearIcon />
+                </IconButton>
+              </div>
+            </div>
           ))}
       </List>
 
