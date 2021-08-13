5a0b5332eadb9ec49567227cff06f10c52772d7a
Joanna Derks <joanna.derks@schibsted.pl>
2021-08-13 21:04:39 +0200

    classic event add/delete/skip checkpoints

--- apps/client/src/components/CheckpointListItem/CheckpointListItem.tsx
+++ apps/client/src/components/CheckpointListItem/CheckpointListItem.tsx
@@ -3,10 +3,16 @@ import { Checkpoint } from '../../types/Checkpoint';
 import {
   Button,
   Checkbox,
+  createStyles,
   ListItem,
   ListItemIcon,
   ListItemText,
+  makeStyles,
+  Theme,
 } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { CREATE_CHECKPOINT, DELETE_CHECKPOINT } from '../../queries';
 
 interface Props {
   id: number;
@@ -14,22 +20,122 @@ interface Props {
   eventId: number;
 }
 
+interface Values {
+  eventId: number;
+  cpId: number;
+  cpCode?: string;
+  skipped?: boolean;
+  skipReason?: string;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    skippedText: {
+      fontStyle: 'italic',
+    },
+  })
+);
+
 const CheckpointListItem = ({ id, checkpoint, eventId }: Props) => {
   const labelId = `checkbox-list-label-${id}`;
 
+  const history = useHistory();
+
+  const [createCheckpoint, { loading: creationLoading, error: creationError }] =
+    useMutation<any, Values>(CREATE_CHECKPOINT, {
+      refetchQueries: ['getEvent'],
+      awaitRefetchQueries: true,
+      onCompleted: (data) => {
+        history.push(`/events/${data.createCheckpoint.checkpoint.event_id}`);
+      },
+    });
+
+  const [deleteCheckpoint] = useMutation(DELETE_CHECKPOINT, {
+    refetchQueries: ['getEvent'],
+    awaitRefetchQueries: true,
+  });
+
+  const handleToggle = (value: number) => {
+    if (checkpoint) {
+      return deleteCheckpoint({ variables: { id: checkpoint.id } });
+    }
+
+    try {
+      return createCheckpoint({
+        variables: {
+          eventId,
+          cpId: value,
+          cpCode: undefined,
+          skipped: false,
+          skipReason: undefined,
+        },
+      });
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const handleSkipClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    checkpointId: number
+  ) => {
+    e.stopPropagation();
+
+    try {
+      return createCheckpoint({
+        variables: {
+          eventId,
+          cpId: checkpointId,
+          cpCode: undefined,
+          skipped: true,
+          skipReason: undefined,
+        },
+      });
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const classes = useStyles();
+
+  if (creationLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (creationError) {
+    return <p>Error :(</p>;
+  }
+
+  const skipButton = !checkpoint && (
+    <Button variant="contained" onClick={(e) => handleSkipClick(e, id)}>
+      Skip
+    </Button>
+  );
+
+  const skippedText = checkpoint && checkpoint.skipped && (
+    <span className={classes.skippedText}>skipped</span>
+  );
+
   return (
-    <ListItem key={id} role={undefined} dense button onClick={handleToggle(p)}>
+    <ListItem
+      key={id}
+      role={undefined}
+      dense
+      button
+      onClick={() => handleToggle(id)}
+    >
       <ListItemIcon>
         <Checkbox
           edge="start"
-          checked={checked.indexOf(p) !== -1}
+          checked={checkpoint != null}
           tabIndex={-1}
           disableRipple
           inputProps={{ 'aria-labelledby': labelId }}
         />
       </ListItemIcon>
-      <ListItemText id={labelId} primary={p} />
-      <Button variant="contained">Skip</Button>
+      <ListItemText id={labelId} primary={id} />
+      {skippedText}
+      {skipButton}
     </ListItem>
   );
 };
--- apps/client/src/components/Classic/Classic.tsx
+++ apps/client/src/components/Classic/Classic.tsx
@@ -1,19 +1,4 @@
-import {
-  Box,
-  Button,
-  Checkbox,
-  createStyles,
-  Fab,
-  Grid,
-  GridSpacing,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  makeStyles,
-  Paper,
-  Theme,
-  Typography,
-} from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import React from 'react';
 import { OrienteeringEvent } from '../../types/OrienteeringEvent';
 import MissingCheckpointsArea from '../MissingCheckpointsArea/MissingCheckpointsArea';
@@ -24,53 +9,9 @@ interface Props {
   event: OrienteeringEvent;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      padding: 20,
-    },
-    paper: {
-      width: 80,
-    },
-    item: {
-      padding: 5,
-    },
-    control: {
-      padding: theme.spacing(2),
-    },
-    addCheckpointButton: {
-      position: 'absolute',
-      right: '1em',
-      bottom: '1em',
-    },
-  })
-);
-
 const Classic = ({ event }: Props) => {
   const { id, name, checkpoints, checkpoint_count } = event;
 
-  const [spacing] = React.useState<GridSpacing>(2);
-
-  const classes = useStyles();
-
-  const [checked, setChecked] = React.useState([0]);
-
-  const handleToggle = (value: number) => () => {
-    console.log('toggle', value);
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
-  console.log('event', event);
   const allPoints = Array(checkpoint_count)
     .fill(checkpoint_count)
     .map((_, idx) => 1 + idx);
@@ -86,26 +27,12 @@ const Classic = ({ event }: Props) => {
         const matchingPoint = checkpoints.find((ch) => ch.cp_id === p);
 
         return (
-          <CheckpointListItem id={p} checkpoint={matchingPoint} eventId={id} />
-          // <ListItem
-          //   key={p}
-          //   role={undefined}
-          //   dense
-          //   button
-          //   onClick={handleToggle(p)}
-          // >
-          //   <ListItemIcon>
-          //     <Checkbox
-          //       edge="start"
-          //       checked={checked.indexOf(p) !== -1}
-          //       tabIndex={-1}
-          //       disableRipple
-          //       inputProps={{ 'aria-labelledby': labelId }}
-          //     />
-          //   </ListItemIcon>
-          //   <ListItemText id={labelId} primary={p} />
-          //   <Button variant="contained">Skip</Button>
-          // </ListItem>
+          <CheckpointListItem
+            id={p}
+            key={p}
+            checkpoint={matchingPoint}
+            eventId={id}
+          />
         );
       })}
 
