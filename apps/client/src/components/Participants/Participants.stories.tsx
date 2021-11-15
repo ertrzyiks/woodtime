2bce85aa803c385a324db2fc96620ab3939d1da2
Mateusz Derks <mateusz.derks@gmail.com>
2021-11-15 22:02:28 +0100

    Add more stories (#73)

---
+++ apps/client/src/components/Participants/Participants.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Participants from './Participants';
+import {MemoryRouter} from "react-router-dom";
+
+export default {
+  title: 'Components/Participants',
+  component: Participants,
+  decorators: [
+    (Story: React.ComponentType) => (
+      <MemoryRouter initialEntries={['/']} initialIndex={0}>
+        <Story />
+      </MemoryRouter>
+    )
+  ]
+}
+
+interface Args {
+  list: {id: string, name: string}[]
+  eventId: number
+}
+
+export const Default = (args: Args) => <Participants {...args} />
+
+Default.args = {
+  list: [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Alice' },
+  ],
+  eventId: 12
+}
