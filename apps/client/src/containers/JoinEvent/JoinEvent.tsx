8722b4a687175d48e8019d75d7127016ee4791ce
ertrzyiks <mateusz.derks@gmail.com>
2021-11-28 14:50:19 +0100

    Update cache after event join

--- apps/client/src/containers/JoinEvent/JoinEvent.tsx
+++ apps/client/src/containers/JoinEvent/JoinEvent.tsx
@@ -3,6 +3,7 @@ import {useMutation} from "@apollo/client";
 import {useHistory, useLocation, useParams} from 'react-router-dom'
 
 import {JoinEventDocument} from "./data/joinEvent";
+import {EventForListFragmentDoc} from "../../queries/eventForListFragment";
 
 const JoinEvent = () => {
   const { id } = useParams<{ id: string }>()
@@ -14,6 +15,24 @@ const JoinEvent = () => {
     variables: {
       id: parseInt(id, 10),
       token: urlParams.get('token')
+    },
+    update: (cache, { data }) => {
+      if (!data?.joinEvent?.event) {
+        return
+      }
+
+      cache.modify({
+        fields: {
+          events: (existingEvents = []) => {
+            const newEventRef = cache.writeFragment({
+              data: data?.joinEvent?.event,
+              fragment: EventForListFragmentDoc
+            })
+
+            return [...existingEvents, newEventRef]
+          }
+        }
+      })
     },
     onCompleted: (data) => {
       if (data?.joinEvent?.success) {
