c322b17fb428cb9afb77e5f266d3dddb099823f3
ertrzyiks <mateusz.derks@gmail.com>
2025-12-12 16:11:22 +0100

    Add 404.html

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -6,5 +6,15 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-index-to-404',
+      apply: 'build',
+      closeBundle() {
+        const fs = require('fs');
+        fs.copyFileSync('dist/index.html', 'dist/404.html');
+      },
+    },
+  ],
 });
