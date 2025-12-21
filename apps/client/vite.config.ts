9f76c82fda664dee0975132acee4f9759ed0a4e7
ertrzyiks <mateusz.derks@gmail.com>
2025-12-21 21:07:11 +0100

    Add pwa plugin

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
+
+const isPWAEnabled = process.env.VITE_ENABLE_PWA === 'true';
+
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
     react(),
+    isPWAEnabled &&
+      VitePWA({
+        registerType: 'autoUpdate',
+        strategies: 'generateSW',
+        filename: 'service-worker.js',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        },
+      }),
     {
       name: 'copy-index-to-404',
       apply: 'build',
