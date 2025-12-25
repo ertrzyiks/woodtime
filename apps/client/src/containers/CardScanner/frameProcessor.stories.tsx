e74b9601c11cef5d3675c52291f6facc2fec7c10
ertrzyiks <mateusz.derks@gmail.com>
2025-12-25 08:04:51 +0100

    Migrate to story

---
+++ apps/client/src/containers/CardScanner/frameProcessor.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { processVideoFrame } from './frameProcessor';
+
+const FrameProcessorSample = () => {
+  return <div>AA</div>;
+};
+
+export default {
+  title: 'Pages/CardScanner/frameProcessor',
+  component: FrameProcessorSample,
+  decorators: [],
+  loaders: [],
+};
+
+export const Default = () => {
+  return <FrameProcessorSample />;
+};
